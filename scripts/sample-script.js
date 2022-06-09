ef466420c765783cd799c56a8e3ba786e3e2875e
Xolmer <xolmer@Xolmers-MacBook-Pro.local>
2022-06-09 23:14:38 +0200

    simple minter

---
+++ scripts/sample-script.js
@@ -0,0 +1,16 @@
+const hre = require("hardhat");
+
+async function main() {
+  const Minter = await hre.ethers.getContractFactory("Minter");
+  const minter = await Minter.deploy("Xolmer", "XLMR");
+
+  await minter.deployed();
+
+  console.log("Minter deployed to:", minter.address);
+}
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
--- src/App.js
+++ src/App.js
@@ -1,23 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import minterAbi from "./minterAbi.json";
+import { ethers, BigNumber } from "ethers";
+import { useEffect, useState } from "react";
+const minterAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3";
 
 function App() {
+  //Connecting
+  const [accounts, setAccounts] = useState([]);
+
+  const connectAccounts = async () => {
+    if (window.ethereum) {
+      const accounts = await window.ethereum.request({ method: "eth_requestAccounts" });
+      setAccounts(accounts);
+    }
+  };
+  useEffect(() => {
+    connectAccounts();
+  }, []);
+
+  //Minting
+  const [mintAmount, setMintAmount] = useState(1);
+  const handleMint = async () => {
+    if (window.ethereum) {
+      const provider = new ethers.providers.Web3Provider(window.ethereum);
+      const signer = await provider.getSigner();
+      const contract = new ethers.Contract(minterAddress, minterAbi.abi, signer);
+      try {
+        const response = await contract.mint(BigNumber.from(mintAmount));
+        console.log("response : ", response);
+      } catch (error) {
+        console.error(error);
+      }
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Minter</h1>
+      {accounts.length && (
+        <div>
+          Mint Button
+          <button onClick={() => setMintAmount(mintAmount - 1)}>-</button>
+          <span>{mintAmount}</span>
+          <button onClick={() => setMintAmount(mintAmount + 1)}>+</button>
+          <button onClick={handleMint}>Mint</button>
+        </div>
+      )}
     </div>
   );
 }
